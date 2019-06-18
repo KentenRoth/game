b77172c179ff9202d4ae68f183e0091d992f1d2f
Kent Roth <kentencroth@yahoo.com>
2019-06-18 06:12:02 -0500

    WIP: Added different outcomes to the battleCry

--- character.js
+++ character.js
@@ -1,7 +1,7 @@
 class Character {
 	constructor(name, weapon) {
 		this.name = name;
-		this.weapon = weapon;
+		this.weapon = weapon.toLowerCase();
 	}
 }
 
@@ -10,15 +10,29 @@ class Player extends Character {
 		super(name, weapon);
 	}
 	battleCry(phrase) {
-		return `As ${
-			this.name
-		} is running into battle they scream ${phrase} with their ${
+		if (this.weapon.includes('bow')) {
+			return `Before screaming ${phrase}, ${this.name} draws their ${
+				this.weapon
+			} to line up a sneak attack`;
+		}
+
+		if (this.weapon.includes('sword')) {
+			return `As ${
+				this.name
+			} is running into battle they scream ${phrase} with their ${
+				this.weapon
+			} drawn.`;
+		}
+
+		return `${this.name} looks down at their ${
 			this.weapon
-		} drawn.`;
+		} in hand before yelling ${phrase} as they run into battle.`;
 	}
+
 	attack() {
 		//TODO needs to have a HP system for damage depending on type of weapon
 	}
 }
 
 const Kent = new Player('Kent', 'Bow');
+console.log(Kent.battleCry('AAAAHHHHH'));
