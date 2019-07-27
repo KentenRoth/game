332804a4cb63ba4551b6f8aa07f42ca1bf91f691
Kent Roth <kentencroth@yahoo.com>
2019-07-27 18:32:42 -0500

    WIP: setting up movement functions

--- PlayGame/play.js
+++ PlayGame/play.js
@@ -7,27 +7,101 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
-rl.question('What direction would you like to go? ', answer => {
-	if (answer === 'west') {
-		console.log(
-			'There is an enemy here.  You can attack by typing attack, you can check his stats by typing stats, or you can go in a different direction by typing North.'
-		);
+const directionalOptions = {
+	cannotGoNorth() {
 		rl.on('line', input => {
-			if (input === 'attack') {
-				play.attack.playerAttack();
+			if (input.toLowercase() === 'north') {
+				return console.log('cannot go North');
 			}
-
-			if (input === 'stats') {
-				play.badGuy.stats();
+		});
+	},
+	cannotGoEast() {
+		rl.on('line', input => {
+			if (input.toLowercase() === 'east') {
+				return console.log('cannot go East');
 			}
-
-			if (input === 'north') {
-				console.log('Went North');
+		});
+	},
+	cannotGoSouth() {
+		rl.on('line', input => {
+			if (input.toLowercase() === 'south') {
+				return console.log('cannot go South');
 			}
+		});
+	},
+	cannotGoWest() {
+		rl.on('line', input => {
+			if (input.toLowercase() === 'west') {
+				return console.log('cannot go west');
+			}
+		});
+	},
+	northEastCorner() {
+		rl.on('line', input => {
+			if (
+				input.toLowercase() === 'north' ||
+				input.toLowercase() === 'east'
+			) {
+				return console.log('you can only move to the South or West');
+			}
+		});
+	},
 
-			if (input === 'exit') {
-				rl.close();
+	northWestCorner() {
+		rl.on('line', input => {
+			if (
+				input.toLowercase() === 'north' ||
+				input.toLowercase() === 'west'
+			) {
+				return console.log('you can only move South or East');
+			}
+		});
+	},
+
+	southEastCorner() {
+		rl.on('line', input => {
+			if (
+				input.toLowercase() === 'south' ||
+				input.toLowercase() === 'east'
+			) {
+				return console.log('you can only move South or West');
+			}
+		});
+	},
+
+	southWestCorner() {
+		rl.on('line', input => {
+			if (
+				input.toLowercase() === 'south' ||
+				input.toLowercase() === 'west'
+			) {
+				return console.log('you can only move South or East');
 			}
 		});
 	}
-});
+};
+
+// rl.question('What direction would you like to go? ', answer => {
+// 	if (answer === 'west') {
+// 		console.log(
+// 			'There is an enemy here.  You can attack by typing attack, you can check his stats by typing stats, or you can go in a different direction by typing North.'
+// 		);
+// 		rl.on('line', input => {
+// 			if (input === 'attack') {
+// 				play.attack.playerAttack();
+// 			}
+
+// 			if (input === 'stats') {
+// 				play.badGuy.stats();
+// 			}
+
+// 			if (input === 'north') {
+// 				console.log('Went North');
+// 			}
+
+// 			if (input === 'exit') {
+// 				rl.close();
+// 			}
+// 		});
+// 	}
+// });
