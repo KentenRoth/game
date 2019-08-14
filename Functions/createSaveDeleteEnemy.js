49868b5bf2ee63af5d8a0800c3ed2283d7e3d9f8
Kent Roth <kentencroth@yahoo.com>
2019-08-14 06:18:18 -0500

    WIP: removing comments

--- Functions/createSaveDeleteEnemy.js
+++ Functions/createSaveDeleteEnemy.js
@@ -21,43 +21,6 @@ const createEnemy = (name, type) => {
 	if (enemy.name === undefined) {
 		enemy.push(new enemyClass[type](name));
 		return saveEnemy(enemy[0]);
-
-		// if (lowerCaseType === 'bug') {
-		// 	enemy.push(new enemyClass.Bug(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'frog') {
-		// 	enemy.push(new enemyClass.Frog(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'bird') {
-		// 	enemy.push(new enemyClass.Bird(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'racoon') {
-		// 	enemy.push(new enemyClass.Racoon(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'crab') {
-		// 	enemy.push(new enemyClass.Crab(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'deer') {
-		// 	enemy.push(new enemyClass.Deer(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'goat') {
-		// 	enemy.push(new enemyClass.Goat(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'lion') {
-		// 	enemy.push(new enemyClass.Lion(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
-		// if (lowerCaseType === 'bigfoot') {
-		// 	enemy.push(new enemyClass.Bigfoot(name));
-		// 	return saveEnemy(enemy[0]);
-		// }
 	}
 };
 
