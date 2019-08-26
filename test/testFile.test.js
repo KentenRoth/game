8e3f8695e38d7ee92d10eadd40d53f53d8d818ea
Kent Roth <kentencroth@yahoo.com>
2019-08-26 18:52:11 -0500

    WIP: Added tests for all enemy styles

--- test/testFile.test.js
+++ test/testFile.test.js
@@ -7,7 +7,7 @@ const {
 	loadEnemy,
 	saveEnemy,
 	createEnemy,
-	delteEnemy
+	deleteEnemy
 } = require('../Functions/createSaveDeleteEnemy');
 
 describe('Create Save Delete Character file', () => {
@@ -28,5 +28,80 @@ describe('Create Save Delete Character file', () => {
 		expect(loadCharacter()).toHaveProperty('name', 'Kent');
 		expect(loadCharacter()).toHaveProperty('type', 'Viking');
 		deleteCharacter();
+	});
+});
+
+describe('Create Save Delete Enemy file', () => {
+	it('should create a new bug enemy', () => {
+		createEnemy('dragon fly', 'Bug');
+		expect(loadEnemy()).toHaveProperty('name', 'dragon fly');
+		expect(loadEnemy()).toHaveProperty('type', 'Bug');
+		deleteEnemy();
+	});
+	it('should create a new frog enemy', () => {
+		createEnemy('bull frog', 'Frog');
+		expect(loadEnemy()).toHaveProperty('name', 'bull frog');
+		expect(loadEnemy()).toHaveProperty('type', 'Frog');
+		deleteEnemy();
+	});
+	it('should create a new from enemy', () => {
+		createEnemy('racoon', 'Racoon');
+		expect(loadEnemy()).toHaveProperty('name', 'racoon');
+		expect(loadEnemy()).toHaveProperty('type', 'Racoon');
+		deleteEnemy();
+	});
+	it('should create a new bird enemy', () => {
+		createEnemy('crow', 'Bird');
+		expect(loadEnemy()).toHaveProperty('name', 'crow');
+		expect(loadEnemy()).toHaveProperty('type', 'Bird');
+		deleteEnemy();
+	});
+	it('should create a new crab enemy', () => {
+		createEnemy('mudcrab', 'Crab');
+		expect(loadEnemy()).toHaveProperty('name', 'mudcrab');
+		expect(loadEnemy()).toHaveProperty('type', 'Crab');
+		deleteEnemy();
+	});
+	it('should create a new snake enemy', () => {
+		createEnemy('bull snake', 'Snake');
+		expect(loadEnemy()).toHaveProperty('name', 'bull snake');
+		expect(loadEnemy()).toHaveProperty('type', 'Snake');
+		deleteEnemy();
+	});
+	it('should create a new fox enemy', () => {
+		createEnemy('fox', 'Fox');
+		expect(loadEnemy()).toHaveProperty('name', 'fox');
+		expect(loadEnemy()).toHaveProperty('type', 'Fox');
+		deleteEnemy();
+	});
+	it('should create a new deer enemy', () => {
+		createEnemy('Bambi', 'Deer');
+		expect(loadEnemy()).toHaveProperty('name', 'Bambi');
+		expect(loadEnemy()).toHaveProperty('type', 'Deer');
+		deleteEnemy();
+	});
+	it('should create a new goat enemy', () => {
+		createEnemy('billy goat', 'Goat');
+		expect(loadEnemy()).toHaveProperty('name', 'billy goat');
+		expect(loadEnemy()).toHaveProperty('type', 'Goat');
+		deleteEnemy();
+	});
+	it('should create a new lion enemy', () => {
+		createEnemy('mountain lion', 'Lion');
+		expect(loadEnemy()).toHaveProperty('name', 'mountain lion');
+		expect(loadEnemy()).toHaveProperty('type', 'Lion');
+		deleteEnemy();
+	});
+	it('should create a new gator enemy', () => {
+		createEnemy('gator', 'Gator');
+		expect(loadEnemy()).toHaveProperty('name', 'gator');
+		expect(loadEnemy()).toHaveProperty('type', 'Gator');
+		deleteEnemy();
+	});
+	it('should create a new bigfoot enemy', () => {
+		createEnemy('BigFoot', 'Bigfoot');
+		expect(loadEnemy()).toHaveProperty('name', 'BigFoot');
+		expect(loadEnemy()).toHaveProperty('type', 'Bigfoot');
+		deleteEnemy();
 	});
 });
